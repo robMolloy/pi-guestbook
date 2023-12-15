a65bcabb39fe3a544236761c3dc77cb892a9ebbd
robmolloyntt <robert.molloy@nttdata.com>
2023-12-15 15:57:13 +0000

    accept square images only

--- api/index.js
+++ api/index.js
@@ -1,18 +1,48 @@
 const express = require("express");
 const fs = require("fs").promises;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sharp = require("sharp");
 
 const port = 3000;
 const app = express();
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
 app.use(cors());
 app.use(bodyParser.json({ limit: "500mb" }));
 
-app.post("/print-image", async (req, res) => {
+const getImageDataFromImageDataUrl = (imageDataUrl) => {
+  return imageDataUrl.split(";base64,")[1];
+};
+
+const getImageDimensionsFromImageDataUrl = (imageDataUrl) => {
+  return new Promise(async (resolve) => {
+    const imageData = getImageDataFromImageDataUrl(imageDataUrl);
+
+    const buffer = Buffer.from(imageData, "base64");
+
+    const metadata = await sharp(buffer).metadata();
+    const imgDims = {
+      width: metadata.width,
+      height: metadata.height,
+      ratio: metadata.height / metadata.width,
+    };
+    resolve(imgDims);
+  });
+};
+
+app.post("/save-square-image", async (req, res) => {
   const imageDataUrl = req.body.image;
-  const imageData = imageDataUrl.split(";base64,")[1];
+  const imageDimensions = await getImageDimensionsFromImageDataUrl(
+    imageDataUrl
+  );
+
+  const isSquare = imageDimensions.ratio > 0.9 || imageDimensions.ratio < 1.1;
+  if (!isSquare)
+    return res
+      .status(500)
+      .json({ success: false, message: "Image is not square" });
+
+  const imageData = getImageDataFromImageDataUrl(imageDataUrl);
   const imageName = `../files/squareImages/${new Date().toISOString()}.png`;
 
   try {
