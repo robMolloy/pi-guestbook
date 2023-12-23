939cf0d483ef177bca35f7ae0c278fe22bbb32da
robmolloyntt <robert.molloy@nttdata.com>
2023-12-23 10:44:15 +0000

    project-structure and serve from api

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ const credentials = { key: privateKey, cert: certificate };
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "500mb" }));
+
 app.use(express.static("../interfaceLocal/dist"));
 
 app.post("/save-square-image", handlers.saveSquareImageHandler);
