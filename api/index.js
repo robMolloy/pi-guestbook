2cff7f70e54909f18538e556bd482ae3f341b996
robmolloyntt <robert.molloy@nttdata.com>
2023-12-23 10:42:02 +0000

    project-structure and serve from api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const fs = require("fs");
-const fsPromises = fs.promises;
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const sharp = require("sharp");
 const https = require("https");
+const handlers = require("./handlers/index");
 
 const port = 3005;
 const app = express();
@@ -15,60 +14,11 @@ const credentials = { key: privateKey, cert: certificate };
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "500mb" }));
+app.use(express.static("../interfaceLocal/dist"));
 
-const getImageDataFromImageDataUrl = (imageDataUrl) => {
-  return imageDataUrl.split(";base64,")[1];
-};
+app.post("/save-square-image", handlers.saveSquareImageHandler);
 
-const getImageDimensionsFromImageDataUrl = (imageDataUrl) => {
-  return new Promise(async (resolve) => {
-    const imageData = getImageDataFromImageDataUrl(imageDataUrl);
-
-    const buffer = Buffer.from(imageData, "base64");
-
-    const metadata = await sharp(buffer).metadata();
-    const imgDims = {
-      width: metadata.width,
-      height: metadata.height,
-      ratio: metadata.height / metadata.width,
-    };
-
-    resolve(imgDims);
-  });
-};
-
-app.get("/", async (req, res) => {
-  return res
-    .status(500)
-    .json({ success: false, message: "Image is not square" });
-});
-
-app.post("/save-square-image", async (req, res) => {
-  const imageDataUrl = req.body.image;
-  const imageDimensions = await getImageDimensionsFromImageDataUrl(
-    imageDataUrl
-  );
-
-  const isSquare = imageDimensions.ratio > 0.9 || imageDimensions.ratio < 1.1;
-  if (!isSquare)
-    return res
-      .status(500)
-      .json({ success: false, message: "Image is not square" });
-
-  const imageData = getImageDataFromImageDataUrl(imageDataUrl);
-  const imageName = `../files/squareImages/${new Date().toISOString()}.png`;
-
-  try {
-    await fsPromises.writeFile(imageName, imageData, "base64");
-    console.log(`${imageName} uploaded successfully`);
-    res
-      .status(200)
-      .json({ success: true, message: "File uploaded successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "Error writing the file" });
-  }
-});
+app.post("/save-pdf-print-image", handlers.savePdfPrintImageHandler);
 
 // Create HTTPS server
 const httpsServer = https.createServer(credentials, app);
